b29a8359aed6b74f7717778170f81ff42394f4dc
KimWonGyu <m55f@naver.com>
2019-12-17 02:42:16 +0900

    add auto-playing server

--- MinecraftGit.py
+++ MinecraftGit.py
@@ -1,8 +1,26 @@
 import subprocess
+import time
 
 directory = 'bedrock-server/'
 program = 'bedrock_server.exe'
-#subprocess.call(['powershell.exe', './' + directory + program])
+
 p = subprocess.Popen('powershell.exe', stdin=subprocess.PIPE)
+p.stdin.write(bytes('cd ' + directory + '\n', encoding='utf8'))
+p.stdin.flush()
+
+p.stdin.write(bytes('./' + program + '\n', encoding='utf8'))
+p.stdin.flush()
+
+#p.communicate(bytes('cd ' + directory, encoding='utf8'))
+
+#p = subprocess.call('powershell.exe', stdin=subprocess.PIPE)
+
+
+
+#p = subprocess.Popen('powershell.exe', stdin=subprocess.PIPE)
+#p.communicate(bytes('./' + program, encoding='utf8'))
+
+#p.stdin.write(bytearray(['cd ' + directory]))
+#p.communicate()
 #out = p.communicate(input='cd ' + directory)
-p.stdin.write(['cd', directory])
+#p.stdin.write(['cd', directory])
